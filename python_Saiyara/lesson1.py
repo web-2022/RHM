313bf98ab88807c883596df38cc21815909ea0f2
web2022 <victorgamarra2910@hotmail.com>
2025-05-18 13:15:23 -0500

    Block

---
+++ python_Saiyara/lesson1.py
@@ -0,0 +1,4 @@
+# Lesson 1
+name = input("What is your name")
+print("Nice to meet you, "+ name+"!")
+
---
+++ python_Saiyara_print.py
@@ -0,0 +1,26 @@
+# # Lesson 1: print
+# name = input("What is your name")
+# print("Nice to meet you, "+ name+"!")
+
+# age= input("How old are you")
+# print("You are "+age+" years old")
+
+# city = input("Where are you from?")
+# print("You are from "+city+"!")
+
+# Mini excercise
+#1
+color = input("What is your favority color?")
+print("Your favority color is "+color+"!")
+
+#2
+name = input("What is your name")
+print("Your name is "+ name+".")
+
+#3
+job = input("what is your job")
+print("Your job is "+job)
+
+#4
+question = input("A que le tienes miedo")
+print("Le tienes miedo a "+question)
